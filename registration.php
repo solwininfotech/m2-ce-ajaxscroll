44b2c8d9ac11d27f395ee6fc2f4f00cec80ea216
Your Name <mmdhudasia@gmail.com>
2020-07-30 10:42:09 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+ /**
+  * Solwin Infotech
+  * Solwin AjaxScroll Extension
+  *
+  * @category   Solwin
+  * @package    Solwin_AjaxScroll
+  * @copyright  Copyright © 2006-2017 Solwin (https://www.solwininfotech.com)
+  * @license    https://www.solwininfotech.com/magento-extension-license/
+  */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_AjaxScroll',
+    __DIR__
+);
